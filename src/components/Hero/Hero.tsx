cc84fb671c9cae717cf394ef5c9c7ab37fcc8405
Luis Gerardo Cardozo Carranza <78442724+luiscardozo161002@users.noreply.github.com>
2026-03-10 16:23:26 -0600

    Update Hero.tsx

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,5 +1,5 @@
 import { motion, useScroll, useTransform } from "framer-motion";
-import { Phone, Mail, MapPin, ChevronDown } from "lucide-react";
+import { Phone, Mail, MapPin } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
 
